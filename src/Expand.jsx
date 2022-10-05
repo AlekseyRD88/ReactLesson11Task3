7ada6b700d11ac3555885387f3d5e509acfc581b
= <neymegen50@gmail.com>
2022-10-05 15:38:27 +0300

    message

--- src/Expand.jsx
+++ src/Expand.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 
-const Expand = ({ children, title}) => {
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn"><i className="fas fa-chevron-up"></i></button>
+const Expand = ({ isOpen, children, title}) => {
+  if(!isOpen) {
+    return(
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn"><i className="fas fa-chevron-down" onClick={this.showText}></i></button>
+        </div>
       </div>
-      <div className="expand__content">{children}</div>
-    </div>
-    
-  );
+    );   
+  }
+  else if (isOpen) {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn"><i className="fas fa-chevron-down" onClick={this.hideText}></i></button>
+        </div>
+        <div className="expand__content">{children}</div>
+      </div>
+      
+    );
+  }
 };
 
 export default Expand;
